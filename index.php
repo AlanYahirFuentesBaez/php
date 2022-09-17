8e87e04fb8e938cd3e7bbf6e137d01e8bc4774b9
and <surftware@gmail.com>
2022-09-17 17:04:45 -0600

    variables y condicionales php

--- index.php
+++ index.php
@@ -10,8 +10,45 @@
 </head>
 
 <body>
+    <p>Que onda soy un mensaje</p>
+    <?php
+        
+        $yahirRoba=400;
+        $yahirLeDaMama=10;
+        $sumaDineroYAhir=$yahirLeDaMama+$yahirRoba;
+
+        echo ("El total de Yahir es:");
+        echo $sumaDineroYAhir;
+
+        $yoRobo=2000;
+        $miMedan=30;
+
+        $miSuma=$yoRobo+$miMedan;
+
+        echo ("<br>Mi total es:");
+        echo $miSuma;
+
+
+        $migueRoba=300;
+        $migueLeDaMama=1000;
+
+        $sumaMigue=$migueRoba+$migueLeDaMama;
+        echo ("<br>El total de Migue es:");
+        echo $sumaMigue;
+
+        if($miSuma>$sumaDineroYAhir && $miSuma>$sumaMigue)
+            echo"<br>Andrés se dispara las papas y los refrescos";
+        else if($sumaDineroYAhir > $miSuma && $sumaDineroYAhir > $sumaMigue)
+            echo"<br>Yahir se dispara las papas y los refrescos";
+        else if($sumaMigue> $miSuma && $sumaMigue > $sumaDineroYAhir)
+            echo"<br>Migue se dispara las papas y los refrescos";
+            
+
+
+
+    ?>
     <h1>Genera una URL de búsqueda</h1>
-    <input id="bsq" type="text" name="busqueda" placeholder="Buscar...">    
+    <input id="bsq" type="text" name="busqueda" placeholder="Buscar...">
     <select id="orderBy" name="select">
         <option value="orderBy=5">Lo más nuevo primero</option>
         <option value="orderBy=4">Mayor a menor precio</option>
@@ -22,7 +59,7 @@
         <option value="pageSize=24">Mostrar 24 elemento</option>
         <option value="pageSize=36">Mostrar 36 elemento</option>
     </select>
-      <input type="submit" value="Generar" onclick="generaUrl()">
+    <input type="submit" value="Generar" onclick="generaUrl()">
 
     <div id="resultado">
     </div>
@@ -31,11 +68,13 @@
 
     <script>
         function generaUrl() {
-            var terminoBusqueda=document.getElementById("bsq").value;
-            var orden=document.getElementById("orderBy").value;
-            var numElementos=document.getElementById("pageSize").value;
-            document.getElementById('resultado').innerHTML='<p>https://dpstreet.mx/bsq/'+terminoBusqueda+'?'+orden+'&'+numElementos+'</p>'; 
+            var terminoBusqueda = document.getElementById("bsq").value;
+            var orden = document.getElementById("orderBy").value;
+            var numElementos = document.getElementById("pageSize").value;
+            document.getElementById('resultado').innerHTML = '<p>https://dpstreet.mx/bsq/' + terminoBusqueda + '?' +
+                orden + '&' + numElementos + '</p>';
         }
     </script>
 </body>
+
 </html>
