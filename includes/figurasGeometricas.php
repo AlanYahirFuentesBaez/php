adf877b95d6ed3fa037757a4f5cb4608e3436830
and <surftware@gmail.com>
2022-09-25 21:18:50 -0600

    comienzo de if

--- includes/figurasGeometricas.php
+++ includes/figurasGeometricas.php
@@ -9,47 +9,56 @@
     <input type="submit" value="Calcular Figura">
 </form>
 <?php
+include("functions/math.php");
+
 echo "$_POST[base]";
 echo "$_POST[altura]";
 echo "$_POST[figura]";
 
+$seleccionFigura=$_POST[figura];
+
+if($_POST[figura]=="cuad"){
+    $numLadosCuadrado=4;
+    $_POST[base]=$lado;
+    $perimetroCuadrado=perimetro($numLadosCuadrado,$lado);
+    $areaCuadrado=area($lado,$lado);
+    echo "El area del cuadrado es: $areaCuadrado ";
+    echo "<br>";
+    echo "El perimetro del cuadrado es: $perimetroCuadrado";
+
+}
+else if($_POST[figura]=="rect"){
+    //Programa del rectangulo(area y perimetro)
+    $numLadosRectangulo=2;
+    $_POST[altura]=$ladoDerecho;
+    $_POST[base]=$base;
+    
+    $ladoRectangulo=$base+$ladoDerecho;
+    $perimetroRectangulo=perimetro($numLadosRectangulo,$ladoRectangulo);
+    $areaRectangulo=area($base,$ladoDerecho);
+    echo "<br><br>";
+    echo "El area del Rectangulo es: $areaRectangulo ";
+    echo "<br>";
+    echo "El perimetro del Rectangulo es: $perimetroRectangulo";
+
+}
+else if($_POST[figura]=="trian"){
+    //programatriangulo(area y perimetro)
+    $numLadosTriangulo=3;
+    $_POST[altura]=$alturaTriangulo;
+    $_POST[base]=$base;
+    $perimetroTriangulo=perimetro($numLadosTriangulo,$base);    
+    $areaTriangulo=areaTriangulo($base,$alturaTriangulo);
+    echo "<br><br>";
+    echo "El area del triangulo es: $areaTriangulo";
+    echo "<br>";
+    echo "El perimetro del triangulo es: $perimetroTriangulo";
+    
+}
+
 
 echo "<br>";
-include("functions/math.php");
-$numLadosCuadrado=4;
-$numLadosRectangulo=2;
-$numLadosTriangulo=3;
-
-$lado=10;
-
-$base=15;
-$ladoDerecho=10;
-$ladoRectangulo=$base+$ladoDerecho;
-
-$alturaTriangulo=13;
 
 
 
-$perimetroCuadrado=perimetro($numLadosCuadrado,$lado);
-$areaCuadrado=area($lado,$lado);
-echo "El area del cuadrado es: $areaCuadrado ";
-echo "<br>";
-echo "El perimetro del cuadrado es: $perimetroCuadrado";
-
-//Programa del rectangulo(area y perimetro)
-$perimetroRectangulo=perimetro($numLadosRectangulo,$ladoRectangulo);
-$areaRectangulo=area($base,$ladoDerecho);
-echo "<br><br>";
-echo "El area del Rectangulo es: $areaRectangulo ";
-echo "<br>";
-echo "El perimetro del Rectangulo es: $perimetroRectangulo";
-
-
-//programatriangulo(area y perimetro)
-$perimetroTriangulo=perimetro($numLadosTriangulo,$base);
-$areaTriangulo=areaTriangulo($base,$alturaTriangulo);
-echo "<br><br>";
-echo "El area del triangulo es: $areaTriangulo";
-echo "<br>";
-echo "El perimetro del triangulo es: $perimetroTriangulo";
 ?>
